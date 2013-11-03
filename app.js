85e6c12f6fcfe24bf1f4f08a8441a5dd9e8f4a9a
lejboua <andre.m.de.albuquerque@gmail.com>
2013-11-03 11:47:28 +0000

    Implementado o /test que envia um Hello World por mail

--- app.js
+++ app.js
@@ -37,6 +37,42 @@ app.get('/', function(req, res) {
     res.send("Hello world, I'm the ThatsIt Playlist Manager");
 });
 
+var nodemailer = require("nodemailer");
+
+app.get('/test', function(req, res) {
+    // create reusable transport method (opens pool of SMTP connections)
+    var smtpTransport = nodemailer.createTransport("SMTP", {
+        service: "Gmail",
+        auth: {
+            user: "outdoors.thatsit@gmail.com",
+            pass: "macksodius"
+        }
+    });
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: "Thatsit Outdoors ✔ <outdoors.thatsit@gmail.com>", // sender address
+        to: "andre.m.de.albuquerque@gmail.com, dre_albuquerque@hotmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world ✔", // plaintext body
+        html: ("<b>Hello world ✔</b> at " + (new Date()).toString()) // html body
+    }
+
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+
+        // if you don't want to use this transport object anymore, uncomment following line
+        smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+
+    res.send("Hello world, I'm the ThatsIt Playlist Manager. Sent mail!");
+});
+
 app.listen(config.web.port);
 logger.info('Listening on port ' + config.web.port);
 logger.info('Running on: ' + process.cwd());
